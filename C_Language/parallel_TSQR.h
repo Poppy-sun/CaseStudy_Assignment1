2cb196796dc8b6f2661e025b785c652f73185ccd
Poppy-sun <lishuangsun50@gmail.com>
2025-02-21 19:36:00 +0000

    Delete C_Language directory

--- C_Language/parallel_TSQR.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARALLEL_TSQR_H
-#define PARALLEL_TSQR_H
-
-void parallel_TSQR(double *A_local, int m_local, int n);
-void matrix_multiply(double *A, double *B, double *C, int m, int n, int p);
-double frobenius_norm(double *A, int m, int n);
-void compute_orthogonality(double *Q, int m, int n, double *QtQ);
-
-#endif /* PARALLEL_TSQR_H */
